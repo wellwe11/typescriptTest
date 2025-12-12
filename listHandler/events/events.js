3fc9f3d4f8668c3978036e46cf199af233663fa3
Robin Ryan <robin1ryan@hotmail.com>
2025-12-12 15:09:58 +0200

    list now works with adding input

--- listHandler/events/events.js
+++ listHandler/events/events.js
@@ -1,2 +1,13 @@
+import handleInput from "../handleInput/handleInput.js"; // stores input and clears it
+import appendListItem from "../appendListItem/appendListItem.js"; // adds new list-item to list
 import findElement from "../abstract/functions/findElement/findElement.js";
-const inputElement = findElement("#taskInput"), listUl = findElement("#todoList"), UlElement = findElement("#todoList"), form = findElement("taskForm");
+const inputElement = findElement("#taskInput"), listUl = findElement("#todoList"), form = findElement("#taskForm");
+function handleSubmit(e) {
+    const input = handleInput(e, inputElement);
+    if (!input)
+        return;
+    appendListItem(listUl, input);
+}
+if (form) {
+    form.addEventListener("submit", handleSubmit);
+}
--- listHandler/events/events.ts
+++ listHandler/events/events.ts
@@ -1,9 +1,19 @@
-import handleInput from "../handleInput/handleInput"; // stores input and clears it
-import handle_ul from "../addListToHtml/addListToHtml"; // adds new list-item to list
+import handleInput from "../handleInput/handleInput.js"; // stores input and clears it
+import appendListItem from "../appendListItem/appendListItem.js"; // adds new list-item to list
 
 import findElement from "../abstract/functions/findElement/findElement.js";
 
-const inputElement = findElement("#taskInput"),
-  listUl = findElement("#todoList"),
-  UlElement = findElement("#todoList"),
-  form = findElement("taskForm");
+const inputElement = findElement("#taskInput") as HTMLInputElement,
+  listUl = findElement("#todoList") as HTMLUListElement,
+  form = findElement("#taskForm") as HTMLFormElement;
+
+function handleSubmit(e: SubmitEvent) {
+  const input = handleInput(e, inputElement);
+  if (!input) return;
+
+  appendListItem(listUl, input);
+}
+
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
